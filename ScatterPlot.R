3d1829f9264b580ac5b5c3e84a5f8e921379c6ae
JAEKYUNG SHIN <34096001+J1205@users.noreply.github.com>
2018-10-13 19:52:04 +0900

    Update ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -2,7 +2,7 @@ library(tidyverse)
 attach(mtcars)
 
 ggplot(data=mtcars, aes(x = wt, y = mpg)) + 
-    geom_point(shape = 19, size = 2.5, colour = cyl) + # shape 19: solid circle
+    geom_point(shape = 19, size = 2.5, colour = cyl) + 
     ggtitle("Scatter Plot: MPG vs. Weight with Model Label") + 
-    geom_text(aes(label=row.names(mtcars), vjust=-1), check_overlap = T, size = 3) + # vjust=-1 위로, hjust=1 오른쪽 
+    geom_text(aes(label=row.names(mtcars), vjust=-1), check_overlap = T, size = 3) + 
     theme_bw()
